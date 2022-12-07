622e052240066b57d768842dd9dbc1f1d2b292b8
doldol123 <toruig59@gmail.com>
2022-12-06 17:05:11 -0800

    create queires

---
+++ client/src/utils/queries.js
@@ -0,0 +1,35 @@
+import { gql } from '@apollo/client';
+
+export const QUERY_USER = gql `
+query user {
+    user {
+        _id
+        username
+        email
+        bookCount
+        savedBooks
+    }
+}
+`;
+
+export const QUERY_SINGLE_USER = gql `
+    query singleUser($userId: ID!) {
+        _id
+        username
+        email
+        bookCount
+        savedBooks
+    }
+`;
+
+export const QUERY_ME = gql `
+    query me {
+        me {
+         _id
+        username
+        email
+        bookCount
+        savedBooks 
+        }
+    }
+`
