697ff86b20f8f2e62f53227d777792ca940f8088
doldol123 <toruig59@gmail.com>
2022-12-06 17:39:31 -0800

    revise APP.js in src

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,39 @@
 import React from 'react';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
+const httplink = createHttpLink({
+  uri: 'graphql',
+});
+
+const authLink = setContext ((_, { navbars }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    navbars: {
+      ...navbars,
+      authorization: token? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httplink),
+  cache: new InMemoryCache(),
+});
+
+
 function App() {
   return (
+    <ApolloProvider client={client}>
     <Router>
       <>
         <Navbar />
@@ -16,6 +44,7 @@ function App() {
         </Switch>
       </>
     </Router>
+     </ApolloProvider>
   );
 }
 
